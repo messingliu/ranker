3f87e8ad27af4fbc5d7339d07df144c422e1498d
pendesen <pendesen@163.com>
2018-04-20 13:17:59 +0800

    new hbase

--- src/main/java/com/tantan/ranker/bean/Feature.java
+++ src/main/java/com/tantan/ranker/bean/Feature.java
@@ -2,8 +2,8 @@ package com.tantan.ranker.bean;
 
 public class Feature {
     private String rowId;
-    private java.lang.String device_os_name;
-    private java.lang.String device_app_version;
+    private String device_os_name;
+    private String device_app_version;
     private double rate_received_like_over_impression_latest_7_days;
     private double rate_giving_like_over_impression_latest_7_days;
     private double rate_match_over_impression_latest_7_days;
@@ -20,10 +20,45 @@ public class Feature {
     private int mlc_type;
     private int age;
     private int constellation;
-    private java.lang.String gender;
+    private String gender;
     private int city;
     private int is_vip;
-    private java.lang.String spam_status;
+    private String spam_status;
+
+    private String birthdate;
+    private String company;
+    private String country_code;
+    private String created_time;
+    private String department;
+    private String description;
+    private String dt;
+    private String hangouts;
+    private String hide_contacts;
+    private String hide_mutual_contacts;
+    private String hometown;
+    private String industry;
+    private String intent;
+    private String job;
+    private String looking_for_gender;
+    private String major;
+    private String mobile_number;
+    private String name;
+    private String occupation;
+    private String password;
+    private String preview_push_message;
+    private String region_ids;
+    private String scenario_ids;
+    private String school;
+    private int search_max_age;
+    private int search_min_age;
+    private int search_radius;
+    private String show_moment_likes;
+    private String status;
+    private String study_active;
+    private String suggest_search_radius;
+    private String updated_time;
+    private String verified_type;
+    private String work_active;
 
     public String getRowId() {
         return rowId;
@@ -207,5 +242,277 @@ public class Feature {
 
     public void setSpam_status(String spam_status) {
         this.spam_status = spam_status;
+    }
+
+    public String getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(String birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public String getCompany() {
+        return company;
+    }
+
+    public void setCompany(String company) {
+        this.company = company;
+    }
+
+    public String getCountry_code() {
+        return country_code;
+    }
+
+    public void setCountry_code(String country_code) {
+        this.country_code = country_code;
+    }
+
+    public String getCreated_time() {
+        return created_time;
+    }
+
+    public void setCreated_time(String created_time) {
+        this.created_time = created_time;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getDt() {
+        return dt;
+    }
+
+    public void setDt(String dt) {
+        this.dt = dt;
+    }
+
+    public String getHangouts() {
+        return hangouts;
+    }
+
+    public void setHangouts(String hangouts) {
+        this.hangouts = hangouts;
+    }
+
+    public String getHide_contacts() {
+        return hide_contacts;
+    }
+
+    public void setHide_contacts(String hide_contacts) {
+        this.hide_contacts = hide_contacts;
+    }
+
+    public String getHide_mutual_contacts() {
+        return hide_mutual_contacts;
+    }
+
+    public void setHide_mutual_contacts(String hide_mutual_contacts) {
+        this.hide_mutual_contacts = hide_mutual_contacts;
+    }
+
+    public String getHometown() {
+        return hometown;
+    }
+
+    public void setHometown(String hometown) {
+        this.hometown = hometown;
+    }
+
+    public String getIndustry() {
+        return industry;
+    }
+
+    public void setIndustry(String industry) {
+        this.industry = industry;
+    }
+
+    public String getIntent() {
+        return intent;
+    }
+
+    public void setIntent(String intent) {
+        this.intent = intent;
+    }
+
+    public String getJob() {
+        return job;
+    }
+
+    public void setJob(String job) {
+        this.job = job;
+    }
+
+    public String getLooking_for_gender() {
+        return looking_for_gender;
+    }
+
+    public void setLooking_for_gender(String looking_for_gender) {
+        this.looking_for_gender = looking_for_gender;
+    }
+
+    public String getMajor() {
+        return major;
+    }
+
+    public void setMajor(String major) {
+        this.major = major;
+    }
+
+    public String getMobile_number() {
+        return mobile_number;
+    }
+
+    public void setMobile_number(String mobile_number) {
+        this.mobile_number = mobile_number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getOccupation() {
+        return occupation;
+    }
+
+    public void setOccupation(String occupation) {
+        this.occupation = occupation;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPreview_push_message() {
+        return preview_push_message;
+    }
+
+    public void setPreview_push_message(String preview_push_message) {
+        this.preview_push_message = preview_push_message;
+    }
+
+    public String getRegion_ids() {
+        return region_ids;
+    }
+
+    public void setRegion_ids(String region_ids) {
+        this.region_ids = region_ids;
+    }
+
+    public String getScenario_ids() {
+        return scenario_ids;
+    }
+
+    public void setScenario_ids(String scenario_ids) {
+        this.scenario_ids = scenario_ids;
+    }
+
+    public String getSchool() {
+        return school;
+    }
+
+    public void setSchool(String school) {
+        this.school = school;
+    }
+
+    public int getSearch_max_age() {
+        return search_max_age;
+    }
+
+    public void setSearch_max_age(int search_max_age) {
+        this.search_max_age = search_max_age;
+    }
+
+    public int getSearch_min_age() {
+        return search_min_age;
+    }
+
+    public void setSearch_min_age(int search_min_age) {
+        this.search_min_age = search_min_age;
+    }
+
+    public int getSearch_radius() {
+        return search_radius;
+    }
+
+    public void setSearch_radius(int search_radius) {
+        this.search_radius = search_radius;
+    }
+
+    public String getShow_moment_likes() {
+        return show_moment_likes;
+    }
+
+    public void setShow_moment_likes(String show_moment_likes) {
+        this.show_moment_likes = show_moment_likes;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStudy_active() {
+        return study_active;
+    }
+
+    public void setStudy_active(String study_active) {
+        this.study_active = study_active;
+    }
+
+    public String getSuggest_search_radius() {
+        return suggest_search_radius;
+    }
+
+    public void setSuggest_search_radius(String suggest_search_radius) {
+        this.suggest_search_radius = suggest_search_radius;
+    }
+
+    public String getUpdated_time() {
+        return updated_time;
+    }
+
+    public void setUpdated_time(String updated_time) {
+        this.updated_time = updated_time;
+    }
+
+    public String getVerified_type() {
+        return verified_type;
+    }
+
+    public void setVerified_type(String verified_type) {
+        this.verified_type = verified_type;
+    }
+
+    public String getWork_active() {
+        return work_active;
+    }
+
+    public void setWork_active(String work_active) {
+        this.work_active = work_active;
     }
 }
--- src/main/java/com/tantan/ranker/controllers/RankerController.java
+++ src/main/java/com/tantan/ranker/controllers/RankerController.java
@@ -1,6 +1,9 @@
 package com.tantan.ranker.controllers;
 
+import com.tantan.ranker.relevance.HBaseFeatureFecther;
 import com.tantan.ranker.services.RankingService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +13,7 @@ import java.util.List;
 
 @RestController
 public class RankerController {
+  private static final Logger LOGGER = LoggerFactory.getLogger(HBaseFeatureFecther.class);
   @Autowired
   private RankingService rankingService;
 
@@ -18,9 +22,15 @@ public class RankerController {
                        @RequestParam(value="candidateIds") List<Long> candidateIds,
                        @RequestParam(value="modelId") int modelId,
                        @RequestParam(value="linearModelParameter", defaultValue = "") String linearModelParameter) {
-    //User user = new User(counter.incrementAndGet(), 1, 2, 3, "here", "type");
-    //RankingService rankingService = new RankingServiceImpl();
-    int topK = 25;
-    return rankingService.getSuggestedUsers(id, candidateIds, modelId, linearModelParameter, topK);
+    try {
+      //User user = new User(counter.incrementAndGet(), 1, 2, 3, "here", "type");
+      //RankingService rankingService = new RankingServiceImpl();
+      int topK = 25;
+      return rankingService.getSuggestedUsers(id, candidateIds, modelId, linearModelParameter, topK);
+    } catch (Exception e) {
+      LOGGER.error("Ranker Error id=" + id + ", candidateIds=" + candidateIds, e);
+      throw e;
+    }
   }
+
 }
